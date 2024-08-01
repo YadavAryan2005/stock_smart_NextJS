45e1b681bfa17f7d57ab5c9e6726cb2fb66981b3
YadavAryan2005 <aryanyadav7822900241@gmail.com>
2024-08-01 18:32:13 +0530

    add files

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,6 +3,7 @@ import { ProductI } from "@/model/product";
 import { ChangeEvent, useState } from "react";
 const Search = ({ products }: { products: ProductI[] }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedProduct, setSelectedProduct] = useState<ProductI | null>(null);
   const [filteredProducts, setFilteredProducts] = useState(products);
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -16,28 +17,38 @@ const Search = ({ products }: { products: ProductI[] }) => {
       setFilteredProducts([]);
     }
   };
+  const selected = (product: ProductI) => {
+    setSelectedProduct(product);
+    setFilteredProducts([]);
+    setSearchTerm(product.Name);
+  };
   return (
     <div className='w-full mx-auto mt-20 bg-white px-8 rounded-lg '>
       <h1 className='text-2xl font-bold mb-6 '>Search a Product</h1>
       <div className='relative'>
-        <input
-          type='text'
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className='shadow appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500  '
-          placeholder='Search for a product'
-        />
+        <div className='flex shadow appearance-none border-2 rounded text-gray-700 leading-tight '>
+          <input
+            type='text'
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className='  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:border-blue-500  '
+            placeholder='Search for a product'
+          />
+          <button className='px-4 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-md m-1'>
+            Update
+          </button>
+        </div>
         {filteredProducts.length > 0 && (
           <ul className='absolute z-10 w-full bg-white border border-gray-300 rounded shadow-md mt-1 max-h-60 '>
             {filteredProducts.map((product) => (
               <li
                 key={product._id}
                 className='py-2 rounded-md cursor-pointer flex hover:bg-gray-100 gap-3 justify-between px-3'
-                onClick={() => handleProductSelect(product)}
+                onClick={() => selected(product)}
               >
                 <div className='flex'>
                   <b>{product.Name}</b>
-                  <h1>
+                  <h1 className='hidden md:block'>
                     {"(" +
                       product.Qty +
                       " available for ₹" +
